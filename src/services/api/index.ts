ae15543d106e353817c918367c77f0c2d54a65b6
ruturajpokemon <ruturajpokemon1@gmail.com>
2024-06-19 00:51:24 +0530

    Change API path to one from .env

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,7 @@
 export const getData = async () => {
   try {
     const response = await fetch(
-      "https://6f76-2405-201-1007-9df-34b7-9443-7e47-d3e7.ngrok-free.app/data",
+      `${process.env.REACT_APP_BASE_PATH}/data`,
       {headers: new Headers({
         "ngrok-skip-browser-warning": "69420",
       })}
