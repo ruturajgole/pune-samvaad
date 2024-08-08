9d1d5d225487f461feba7316213195562bc7c731
ruturajpokemon <ruturajpokemon1@gmail.com>
2024-08-08 19:52:57 +0530

    Add Youtube Icon and Fix Index File

--- src/view/lib/index.tsx
+++ src/view/lib/index.tsx
@@ -3,8 +3,13 @@ import { Modal, ModalProps } from "./modal";
 import { Carousel } from "./carousel";
 import { Header } from "./header";
 import { Loader } from "./loader";
+import Footer from "./footer";
 import animate from "./animated";
 import Footer from './footer';
 
+<<<<<<< HEAD
 export { animate, Carousel, Footer, Header, Modal, Loader, Error };
+=======
+export { animate, Carousel, Header, Footer, Modal, Loader, Error };
+>>>>>>> 2d0a762 (Add Youtube Icon and Fix Index File)
 export type { ModalProps };
