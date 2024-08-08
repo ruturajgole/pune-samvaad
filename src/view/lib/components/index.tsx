8b07e898ef31cceb348f629a0318d9525705de8c
ruturajpokemon <ruturajpokemon1@gmail.com>
2024-08-08 16:03:42 +0530

    Add Form and Button

--- src/view/lib/components/index.tsx
+++ src/view/lib/components/index.tsx
@@ -4,5 +4,6 @@ import Image from "./image";
 import Text from "./text";
 import HeaderMobile from "./header-mobile";
 import Title from "./title";
+import Button from "./button";
 
-export { Accordion, Div, HeaderMobile, Image, Text, Title };
+export { Accordion, Button, Div, HeaderMobile, Image, Text, Title };
